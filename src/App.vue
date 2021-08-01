1aa013d540b1bd215f61ffbf62bae83dd30825c8
Nazmul Sheikh <csenazmul77@gmail.com>
2021-08-01 16:36:50 +0600

    Custom Directive- Global-36

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,14 @@
         <keep-alive>
             <component :is="componentToRender"></component>
         </keep-alive>
+
+        <!-- Custom Directive- Global -->
+        <!-- Assign argument -->
+        <!-- <div v-awesome:red="'Hello Nazmul'"></div>
+        <div v-awesome:green="'Hello Ruksi'"></div> -->
+        <!-- assign modifers -->
+        <div v-awesome.red.big="'Hello Nazmul'"></div>
+        <div v-awesome.green.small="'Hello Ruksi'"></div>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,36 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('awesome', {
+  bind(el, binding) {
+    el.innerHTML = binding.value
+    // el.style.color = "Red"
+
+    //asign argument
+    // if(binding.arg === "red"){
+    //   el.style.color = 'red'
+    // }
+    // if(binding.arg === "green"){
+    //   el.style.color = 'green'
+    // }
+    
+    // asign modifiers
+    if(binding.modifiers.green){
+      el.style.color = "green"
+    }
+    if(binding.modifiers.red){
+      el.style.color = "red"
+    }
+    if(binding.modifiers.big){
+      el.style.fontSize = "30px"
+    }
+    if(binding.modifiers.small){
+      el.style.fontSize = "15px"
+    }
+  }
+  
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
